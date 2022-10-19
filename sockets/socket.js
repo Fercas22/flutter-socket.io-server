669eb531d3d51d37415a6e31da32ef6f21a2edfa
Fernando Miguel <fernando.fmet@gmail.com>
2022-10-18 23:46:41 -0500

    Cambios terminados con flutter de las bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,49 @@
-const { io } = require('../index')
+const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Rammstein'));
+bands.addBand(new Band('Enanitos verdes'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado') 
     });
 
     client.on('mensaje', ( payload ) => {
-        console.log('Mensaje', payload);
-
+        // console.log('Mensaje', payload);
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
     });
+
+    client.on('vote-band', (payload) => {
+        // console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        // console.log(payload);
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        // console.log(payload);
+        bands.deleteBands(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     // io.emit('nuevo-mensaje', payload); // Esto emite a todos los clientes conectados
+    //     client.broadcast.emit('nuevo-mensaje', payload); // Esto lo emite a todos menos al cliente que lo envío
+    // });
 });
